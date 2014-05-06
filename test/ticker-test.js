303e46579abbc1bdcf5ba5e3a5ee2eac928d11f6
Josh Harvey <josh@lamassu.is>
2014-05-06 17:26:08 -0400

    Initial commit

--- test/ticker-test.js
+++ test/ticker-test.js
@@ -3,11 +3,13 @@
 var test = require('tap').test;
 var Ticker = require('../lib/ticker').factory();
 
+// Note: BitcoinAverage is rate limited. If you call the test more than once
+// it will error.
 test('Read ticker', function(t){
   Ticker.ticker(['USD', 'EUR', 'ILS'], function(err, results) {
-    t.plan(5 * 3);
+    t.plan(4 * 3 + 1);
+    t.equal(err, null, 'There should be no error');
     results.forEach(function (result) {
-      t.equal(err, null, 'There should be no error');
       t.notEqual(result, null, 'There should be a result');
       t.equal(result.error, undefined, 'The result should contain no error');
       t.notEqual(result.rate, undefined, 'A rate should have been returned');
